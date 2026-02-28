7a717dbff579c4ab37fb5ec2fc0bb79da70f73a4
Dhruv3sood <dhruvsood6@gmail.com>
2026-02-28 15:56:45 +0530

    lottie

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,2 +1,14 @@
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
+
+declare module "lottie-react" {
+  import type { ComponentType } from "react";
+  interface LottieProps {
+    animationData?: object;
+    loop?: boolean;
+    className?: string;
+    [key: string]: unknown;
+  }
+  const Lottie: ComponentType<LottieProps>;
+  export default Lottie;
+}
